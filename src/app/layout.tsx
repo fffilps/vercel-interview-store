721ff43315b02e532a9ddb2d9db0730e2fb86b4c
fffilps <jmariwyatt@gmail.com>
2024-11-11 16:48:16 -0800

    used the updated google fonts technique

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Geist, Geist_Mono } from 'next/font/google'
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// // const geistSans = localFont({
+// //   src: "./fonts/GeistVF.woff",
+// //   variable: "--font-geist-sans",
+// //   weight: "100 900",
+// // });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+const geist = Geist()
+const geistMono = Geist_Mono()
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geist.className} ${geistMono.className} antialiased`}
       >
         {children}
       </body>
